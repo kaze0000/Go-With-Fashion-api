7279928b389ee6853b0c91c46888e1f1c7dbfecf
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-08-11 13:54:24 +0900

    add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8000", "本番環境でのアドレス"
+    origins "http://localhost:8000", "https://web.go-with-fashion.com"
 
     resource "*",
       headers: :any,
